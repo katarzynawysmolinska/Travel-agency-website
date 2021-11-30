9e01344bd85c21014145800932d2f24e8be58a2e
katarzynawysmolinska <katarzyna.wysmolinska1@gmail.com>
2021-11-30 13:26:24 +0100

    katarzynawysmolinska.github.io

--- package_holiday_website.js
+++ package_holiday_website.js
@@ -1,50 +1,48 @@
-
 //slogan otwierający w sekcji1
 
-const slogan = document.querySelector( "div.slogan > p");
-const txt = 'Podaruj sobie trochę wolności...';
+const slogan = document.querySelector("div.slogan > p");
+const txt = "Podaruj sobie trochę luksusu...";
 const time = 60;
-let i = 0
+let i = 0;
 
-
-slogan.textContent = '';
+slogan.textContent = "";
 
 function delay() {
-const typing =  () => {
+	const typing = () => {
+		slogan.textContent += txt[i];
+		i++;
 
+		if (i === txt.length) {
+			clearInterval(typingIntervalId);
+		}
+	};
 
-slogan.textContent += txt[i];
-i++
-
-if(i === txt.length) {
-
-    clearInterval(typingIntervalId)
-}
-}
-
-
-const typingIntervalId =  setInterval(typing, time)
-
+	const typingIntervalId = setInterval(typing, time);
 }
 
 setTimeout(delay, 1300);
 
 // wywołanie animacji w nav poprzez scroll
 
-function startAnimation () {
+function startAnimation() {
+	const scrollHeight = window.scrollY;
+	console.log(scrollHeight);
 
-const scrollHeight = window.scrollY;
-console.log(scrollHeight);
-
-
-const section1Height = document.querySelector('section:nth-of-type(1)').clientHeight;
-const section2Height = document.querySelector('section:nth-of-type(2)').clientHeight;
-const section3Height = document.querySelector('section:nth-of-type(3)').clientHeight;
-const section4Height = document.querySelector('section:nth-of-type(4)').clientHeight;
-const section5Height = document.querySelector('section:nth-of-type(5)').clientHeight;
-
-
+	const section1Height = document.querySelector(
+		"section:nth-of-type(1)"
+	).clientHeight;
+	const section2Height = document.querySelector(
+		"section:nth-of-type(2)"
+	).clientHeight;
+	const section3Height = document.querySelector(
+		"section:nth-of-type(3)"
+	).clientHeight;
+	const section4Height = document.querySelector(
+		"section:nth-of-type(4)"
+	).clientHeight;
+	const section5Height = document.querySelector(
+		"section:nth-of-type(5)"
+	).clientHeight;
 }
 
-
-document.addEventListener('scroll', startAnimation)
+document.addEventListener("scroll", startAnimation);
